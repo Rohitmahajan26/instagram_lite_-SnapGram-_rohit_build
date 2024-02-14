6bdb7208bdefc957bf6caea89eb62681d7e3a789
rohit mahajan <rohitam35@gmail.com>
2024-02-14 22:54:19 +0530

    size change of postcard image

--- ShareSnapchat/src/components/shared/PostCard.tsx
+++ ShareSnapchat/src/components/shared/PostCard.tsx
@@ -25,7 +25,7 @@ const PostCard = ({ post }: PostCardProps) => {
                 "/assets/icons/profile-placeholder.svg"
               }
               alt="creator"
-              className="w-14 lg:h-14 rounded-full"
+              className="w-13 lg:h-18 rounded-full"
             />
           </Link>
 
